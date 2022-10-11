5d27fadab6fff0b2d908d4fb704894388540e1bb
kwaktaemin <kwak9898@github.com>
2022-10-11 11:21:56 +0900

    구현

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import express from "express";
 import { Message } from "./message";
-
 const app = express();
+const port = 3000
+
 app.use(express.json());
+
 let http = require("http").Server(app)
 let io = require("socket.io")(http);
 
 const chat = io.of("/chatRoom");
-chat.on
+chat.on("connection", (socket: any) => {
+    socket.on("Welcome", (message: Message) => {
+        socket.join(message.room);
+        chat.in(message.room).emit("Welcome", message);
+    });
+    socket.on("message", (message: Message) => {
+        chat.in(message.room).emit("message", message);
+    });
+    socket.on("leave", (message: Message) => {
+        socket.leave(message.room);
+        chat.in(message.room).emit("leave", message);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Server Connected localhost:${port}`)
+})
